705e0a1a2e0b1c72c6c4ee232f6345958126fdbd
Christien Alden <christien.alden@zoho.com>
2024-03-21 19:36:13 +0800

    removed Handle error and set/longjmp
    - Now using inplace error messages and boolean returns.

--- test/off.cpp
+++ test/off.cpp
@@ -7,16 +7,5 @@ static jmp_buf jmpbuf;
 
 TEST_CASE("handle_error", "[off]")
 {
-	// interaction between '_setjmp' and C++ object destruction is non-portable
-	// but we need to test within this context
-	#pragma warning(disable: 4611)
-	if (setjmp(jmpbuf) == 1)
-	{
-		const char* msg = GetGlobalErrorMessage();
-		REQUIRE(strcmp(msg, "test") == 0);
-	}
-	else
-	{
-		HandleError(jmpbuf, "test");
-	}
+    REQUIRE( 1 == 1 );
 }
