1e0277b6dc14b6bf84d7891ae5bd723e08793522
Christien Alden <christien.alden@zoho.com>
2024-04-28 13:40:57 +0800

    rework camera documentation and naming naming convention

--- include/ace/render/camera.h
+++ include/ace/render/camera.h
@@ -9,140 +9,178 @@
 extern "C" {
 #endif
 
-typedef enum CameraDirection
-{
-    CameraDirection_NONE     = 0,
-    CameraDirection_FORWARD  = 1,
-    CameraDirection_BACKWARD = 1 << 1,
-    CameraDirection_LEFT     = 1 << 2,
-    CameraDirection_RIGHT    = 1 << 3,
-    CameraDirection_UP       = 1 << 4,
-    CameraDirection_DOWN     = 1 << 5
-} CameraDirection;
-
-// opaque structure
-typedef struct Camera Camera;
+/**
+ * \defgroup camera camera
+ * \brief A camera class for rendering.
+ */
 
 /**
+ * \ingroup camera
+ * \enum camera_direction
+ * \brief The movement directions of the camera.
+ * \details
+ * The movement directions of the camera are a bitfield. The camera can move in multiple
+ * directions at once. The directions are combined using the bitwise OR operator '|'. To unset a
+ * direction use the bitwise AND operator '&' along with the bitwise NOT '~' operator. To unset all
+ * directions set the bitfield to camera_direction_none.
+ * \see camera_set_movement_direction
+ * \see camera_get_movement_direction
+ */
+typedef enum camera_direction
+{
+    camera_direction_none     = 0,
+    camera_direction_forward  = 1,
+    camera_direction_backward = 1 << 1,
+    camera_direction_left     = 1 << 2,
+    camera_direction_right    = 1 << 3,
+    camera_direction_up       = 1 << 4,
+    camera_direction_down     = 1 << 5
+} camera_direction;
+
+/**
+ * \ingroup camera
+ * \struct camera
+ * \brief A basic camera structure.
+ * \details
+ * The camera can be moved in 3D space and rotated. The camera has a position, a front vector, a
+ * right vector, an up vector, a yaw, a pitch, and a movement step. The camera can move in multiple
+ * directions at once. The camera can be moved forward, backward, left, right, up, and down. The
+ * camera can also be rotated around the yaw and pitch axes.
+ */
+typedef struct camera camera;
+
+/**
+ * \ingroup camera
  * \brief Create a new camera.
  * \return A pointer to the camera.
  */
-Camera* camera_create(void);
+camera* camera_create(void);
 /**
+ * \ingroup camera
  * \brief Destroy a camera.
- * \param camera The camera to destroy.
+ * \param[out] cam The camera to destroy.
  */
-void    camera_destroy(Camera* camera);
+void    camera_destroy(camera* cam);
 /**
+ * \ingroup camera
  * \brief Initialize a camera with default values.
- * \param camera The camera to initialize.
+ * \param[out] cam The camera to initialize.
  */
-void    camera_init(Camera* camera);
+void    camera_init(camera* cam);
 /**
+ * \ingroup camera
  * \brief Update the orientation of the camera.
- * \param camera The camera to update.
+ * \param[in,out] cam The camera to update.
  */
-void    camera_update_orientation(Camera* camera);
+void    camera_update_orientation(camera* cam);
 /**
+ * \ingroup camera
  * \brief Update the position of the camera.
- * \param camera The camera to update.
+ * \param[in,out] cam The camera to update.
  */
-void    camera_update_position(Camera* camera);
-
+void    camera_update_position(camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the front vector of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The front vector.
  */
-ac_vec3 camera_get_front(const Camera* camera);
+ac_vec3 camera_get_front(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the right vector of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The right vector.
  */
-ac_vec3 camera_get_right(const Camera* camera);
+ac_vec3 camera_get_right(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the up vector of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The up vector.
  */
-ac_vec3 camera_get_up(const Camera* camera);
-
+ac_vec3 camera_get_up(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the yaw of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The yaw.
  */
-float   camera_get_yaw(const Camera* camera);
+float   camera_get_yaw(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the pitch of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The pitch.
  */
-float   camera_get_pitch(const Camera* camera);
+float   camera_get_pitch(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the position of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The position.
  */
-ac_vec3 camera_get_position(const Camera* camera);
+ac_vec3 camera_get_position(const camera* cam);
 /**
+ * \ingroup camera
  * \brief Get the movement speed of the camera.
- * \param camera The camera.
+ * \param[in] cam The camera.
  * \return The movement step.
  */
-float   camera_get_movement_step(const Camera* camera);
-
+float   camera_get_movement_step(const camera* cam);
 /**
+ * \ingroup camera
+ * \brief Returns the movement direction of the camera.
+ * \param[in] cam The camera.
+ * \return The movement direction bitfield.
+ */
+int     camera_get_movement_direction(const camera* cam);
+/**
+ * \ingroup camera
  * \brief Set the yaw of the camera.
- * \param camera The camera.
- * \param yaw The yaw.
+ * \param[in,out] cam The camera.
+ * \param[in] yaw The yaw.
  * \note The yaw is clamped between 0 and 360 degrees.
  */
-void camera_set_yaw(Camera* camera, float yaw);
+void    camera_set_yaw(camera* cam, float yaw);
 /**
+ * \ingroup camera
  * \brief Set the pitch of the camera.
- * \param camera The camera.
- * \param pitch The pitch.
+ * \param[in,out] cam The camera.
+ * \param[in] pitch The pitch.
  * \note The pitch is clamped between -89 and 89 degrees.
  */
-void camera_set_pitch(Camera* camera, float pitch);
+void    camera_set_pitch(camera* cam, float pitch);
 /**
+ * \ingroup camera
  * \brief Set the position of the camera.
- * \param camera The camera.
- * \param position The position.
+ * \param[in,out] cam The camera.
+ * \param[in] position The position.
  */
-void camera_set_position(Camera* camera, ac_vec3 position);
+void    camera_set_position(camera* cam, ac_vec3 position);
 /**
+ * \ingroup camera
  * \brief Set the movement speed of the camera.
- * \param camera The camera.
- * \param step The movement step.
+ * \param[in,out] cam The camera.
+ * \param[in] step The movement step.
  */
-void camera_set_movement_step(Camera* camera, float step);
-
+void    camera_set_movement_step(camera* cam, float step);
 /**
+ * \ingroup camera
  * \brief Set the movement direction of the camera.
- * \param camera The camera.
- * \param direction The movement direction.
- * \note The direction is a bitfield of CameraDirection.
- * \see CameraDirection
- *
+ * \param[in,out] cam The camera.
+ * \param[in] direction The movement direction bitfield.
+ * \see camera_direction.
  * \details
- * The direction is a bitfield of CameraDirection. The camera will move in the direction of the
+ * The direction is a bitfield of camera_direction. The camera will move in the direction of the
  * bitfield. The camera will continue to move in a given direction until that bit is unset. To set
  * the bit use the bitwise OR operator '|', to unset the bit use the bitwise AND operator '&' along
  * with the bitwise NOT '~' operator. For example, to set the forward and right bits:
- * CameraDirection_FORWARD | CameraDirection_RIGHT.
- * To unset the forward bit: direction & ~CameraDirection_FORWARD
- * You can also unset all bits by setting the direction to CameraDirection_NONE.
+ * camera_direction_forward | camera_direction_right.
+ * To unset the forward bit: direction & ~camera_direction_forward
+ * You can also unset all bits by setting the direction to camera_direction_none.
  */
-void camera_set_movement_direction(Camera* camera, int direction);
-/**
- * \brief Returns the movement direction of the camera.
- * \param camera The camera.
- * \return The movement direction bitfield.
- */
-int  camera_get_movement_direction(Camera* camera);
+void    camera_set_movement_direction(camera* cam, int direction);
 
 #ifdef __cplusplus
 }
