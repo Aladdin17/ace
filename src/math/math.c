7f2c81f7db1cb6db30a46462054242375543d3bc
Christien Alden <christien.alden@zoho.com>
2024-05-02 14:52:16 +0800

    add floating point comparison function to math.h

--- src/math/math.c
+++ src/math/math.c
@@ -3,6 +3,7 @@
  * \brief Math functions implementation.
  */
 #include <ace/math/math.h>
+#include <math.h>
 #define AC_DEG_RAD_SCALAR 180.0f
 
 float ac_deg_to_rad(float degrees)
@@ -20,4 +21,10 @@ float ac_clamp(float value, float min, float max)
 {
     float temp = value < min ? min : value;
     return temp > max ? max : temp;
+}
+
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+bool ac_float_compare(float a, float b, float epsilon)
+{
+    return fabsf(a - b) <= epsilon;
 }
